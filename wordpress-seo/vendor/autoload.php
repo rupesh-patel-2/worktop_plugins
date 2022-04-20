d1feba617e73667ca6e39a89e093291afb1a728d
Rupesh Patel <rupesh.patel@matrixhive.com>
2022-04-20 16:22:20 +0530

    wp seo changes

--- wordpress-seo/vendor/autoload.php
+++ wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitcabd01e8e5c9131326a25a2a4633e747::getLoader();
+return ComposerAutoloaderInit00541031bf5b992ae6401c1513ae208b::getLoader();
